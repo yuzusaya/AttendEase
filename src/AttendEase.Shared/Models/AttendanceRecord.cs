ac9ebba58c6998c46693528d90e9cb4a7b48d554
sia <sia.yukheng@furukawaelectric.cmo>
2024-07-23 17:44:03 +0900

    format: format the ToString of AttendanceRecord

--- src/AttendEase.Shared/Models/AttendanceRecord.cs
+++ src/AttendEase.Shared/Models/AttendanceRecord.cs
@@ -1,5 +1,6 @@
 using AttendEase.Shared.Enums;
 using AttendEase.Shared.Utilities;
+using System.Text;
 
 namespace AttendEase.Shared.Models;
 public record AttendanceRecord
@@ -14,4 +15,16 @@ public record AttendanceRecord
     public DayOfWeek Day => Date.ToDateTime(StartTime).DayOfWeek;
     public TimeOnly SuggestedStartTime => TimeUtility.GetCheckInTimeFromStartTime(StartTime);
     public TimeOnly SuggestedEndTime => TimeUtility.GetCheckOutTimeFromEndTime(EndTime);
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"{Date.ToString("dd/MM")}({Day.ToString()}) - {Status}:");
+        sb.AppendLine($"Time: {StartTime.ToString()} - {EndTime.ToString()}");
+        sb.AppendLine($"Actual Time: {ActualStartTime.ToString()} - {ActualEndTime.ToString()}");
+        if (!string.IsNullOrWhiteSpace(Remarks))
+        {
+            sb.AppendLine($"Remarks: {Remarks}");
+        }
+        return sb.ToString();
+    }
 }
