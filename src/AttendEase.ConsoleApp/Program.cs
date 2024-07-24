dc57160ef5151da92e3040253604c8ce7c4a8ccf
sia <sia.yukheng@furukawaelectric.cmo>
2024-07-24 11:58:55 +0900

    fix: add console readline at the end to prevent termination of program

--- src/AttendEase.ConsoleApp/Program.cs
+++ src/AttendEase.ConsoleApp/Program.cs
@@ -1,9 +1,9 @@
 using AttendEase.Shared.Enums;
 using AttendEase.Shared.Services;
 
-Console.WriteLine("Enter your username:");
+Console.WriteLine("Enter your username (FM-Net):");
 var username = Console.ReadLine();
-Console.WriteLine("Enter your password:");
+Console.WriteLine("Enter your password (FM-Net):");
 var pass = string.Empty;
 ConsoleKey key;
 do
@@ -27,20 +27,31 @@ try
 {
     var currentTime = DateTime.Now;
     IFmNetService fmNetService = new FmNetSeleniumService();
+    IDigiSheetService digiSheetService = new DigiSheetSeleniumService();
     await fmNetService.Login(username, pass);
-    Console.WriteLine("Login successful");
+    Console.WriteLine("Login successfully to FM-net");
     var attendanceRecords = await fmNetService.GetAttandanceRecords(DateOnly.FromDateTime(currentTime));
     var pendingSubmitRecords = attendanceRecords.Where(x => x.Status == AttendanceStatus.NotYetFilled && x.Date < DateOnly.FromDateTime(currentTime));
     var readyToSubmitRecords = pendingSubmitRecords.Where(x => x.Day == DayOfWeek.Saturday || x.Day == DayOfWeek.Sunday || x.SuggestedEndTime != default);
     Console.WriteLine($"{readyToSubmitRecords.Count()}/{pendingSubmitRecords.Count()} ready to submit");
-    foreach (var record in readyToSubmitRecords)
+    if (readyToSubmitRecords.Any())
     {
-        await fmNetService.SubmitAttendance(record.Date, record.SuggestedStartTime, record.SuggestedEndTime);
+        //todo get username and password for digisheet
+        //await digiSheetService.Login("5018", "", pass);
+        foreach (var record in readyToSubmitRecords)
+        {
+            //todo add confirmation?
+            await fmNetService.SubmitAttendance(record.Date, record.SuggestedStartTime, record.SuggestedEndTime);
+            Console.WriteLine($"Submitted {record.Date} to FM-Net");
+            //if success, key in to digisheet as well (if related)
+            //todo get remarks
+            //await digiSheetService.SubmitAttendance(record.Date, record.SuggestedStartTime, record.SuggestedEndTime,"");
+        }
     }
-    // IDigiSheetService digiSheetService = new DigiSheetSeleniumService();
-
+    Console.ReadLine();
 }
 catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
+    Console.ReadLine();
 }
